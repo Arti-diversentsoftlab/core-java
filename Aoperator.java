0dee4c4a67eda4fed17fd9f349ff48c9a7eadc3a
Arti-diversentsoftlab <85992553+Arti-diversentsoftlab@users.noreply.github.com>
2021-06-18 21:05:58 +0530

    Add files via upload

---
+++ Aoperator.java
@@ -0,0 +1,15 @@
+public class Aoperator {
+	public static void main(String[] args) {
+		
+	int m=8,n=8;
+	int a = 2 * ++m; // now a is 18, m is 8
+    int b = 2 * n++; // now b is 16, n is 8
+   System.out.println(a+","+b);
+   int a1= 739156;  
+   int b1 = 5001289;  
+   // Input two values, Output multiplication of a and b  
+   //System.out.println(Math.multiplyExact(a1, b1));  
+   System.out.println(a1*b1);
+
+}
+} 
