66d8c37884fe73f57d69c7e26c4b31ac5ef3b3bd
Arti-diversentsoftlab <85992553+Arti-diversentsoftlab@users.noreply.github.com>
2021-06-18 21:06:44 +0530

    Add files via upload

---
+++ binarynumberules.java
@@ -0,0 +1,12 @@
+public class binarynumberrules {
+public static void main(String[] args) {
+	int  a=45;
+	double b=a;
+	System.out.println(b);//int to double
+	float f=a;
+	System.out.println(f);//int to float
+	long l=a;
+	System.out.println(l);
+	System.out.println(a);
+}
+}
