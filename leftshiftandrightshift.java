3bd477664f96c89a6ecc9f665ee279d4cc8f40fc
Arti-diversentsoftlab <85992553+Arti-diversentsoftlab@users.noreply.github.com>
2021-06-18 21:07:13 +0530

    Add files via upload

---
+++ leftshiftandrightshift.java
@@ -0,0 +1,13 @@
+public class leftshiftandrightshift {
+	public static void main(String[] args) {
+    // a = 5(00000101), b = 9(00001001)
+     int  a = 00000101; 
+    int b = 00001001;
+    // The result is 00001010
+	System.out.println(a<<1 );
+   
+    // The result is 00010010
+    System.out.println(b<<1 ); 
+
+}
+}
