c74339f01d67727c4bb9172108bc37b35ec31474
Arti-diversentsoftlab <85992553+Arti-diversentsoftlab@users.noreply.github.com>
2021-06-28 20:24:58 +0530

    Day 6 Assignment

---
+++ .github3/person.java
@@ -0,0 +1,37 @@
+/*
+ * @author code with z
+ * @version 11
+ * @since 1.5 
+ * 
+ * */
+public class person {
+	/*
+	 * represent name of the person*/
+String name;
+/*
+ * represent age of the person */
+int age;
+/*
+ * @return the name of the person*/
+public String getName() {
+	return name;
+}
+/*
+ * @param name accept the name of the person and set  it on person object*/
+public void setName(String name) {
+	this.name = name;
+}
+/*
+ * @return age of the person
+ * */
+public int getAge() {
+	return age;
+}
+/*
+ * @param age accept the age of person and  it on person object
+ * */
+public void setAge(int age) {
+	this.age = age;
+}
+
+}
